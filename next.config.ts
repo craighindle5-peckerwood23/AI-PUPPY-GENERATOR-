462d698a21d10f9a80454f4c369d28429d8942ae
Craig Hindle JR <247479351+craighindle5-peckerwood23@users.noreply.github.com>
2026-07-28 10:29:45 -0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Force Webpack instead of Turbopack
+  webpack: (config) => {
+    return config;
+  }
 };
 
 export default nextConfig;
