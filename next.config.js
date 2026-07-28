a101911bfa10a561b57f02a6b76059b11049a405
Craig Hindle JR <247479351+craighindle5-peckerwood23@users.noreply.github.com>
2026-07-28 10:30:07 -0700

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Force Webpack instead of Turbopack
-  webpack: (config) => {
-    return config;
-  }
-};
-
-module.exports = nextConfig;
